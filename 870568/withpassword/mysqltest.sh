53d134cb3713f0d4bebbef6ef551da9aac093274
Wolfgang Fahl <wf@bitplan.com>
2017-08-27 10:53:51 +0200

    adds files

---
+++ 870568/withpassword/mysqltest.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# execute the given sql command
+#
+dosql() {
+  local l_sql="$1"
+  local l_db="$2"
+  echo "$l_sql" | mysql -u root -p="$rootpass" $l_db
+}
+
+# get root password from the command line
+rootpass="$1"
+
+# start the mysql daemon
+mysqld_safe --skip-grant-tables&
+sleep 3
+mysql_upgrade --password="$rootpass"
+for table in $(dosql "show tables" mysql)
+do
+  for try in 1 2 3
+  do
+    sql="select count(*) from $table"
+    echo "try #$try for $sql"
+    dosql "$sql" mysql
+  done
+done
+# kill daemon
+echo "killing mysql daemon"
+pkill -f mysqld
+sleep 3
+echo "starting mysql daemon via service"
+service mysql start
+grep ERROR error.log
